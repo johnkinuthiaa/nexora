9ed33bf187ebfe6a64316f595e8548be76b40962
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-03-13 21:47:16 +0300

    added some playlist

--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -1,11 +1,12 @@
-import techStack from "@/utils/techStack";
+import {techStack,socials} from "@/utils/techStack";
 import Image from "next/image";
+import Link from "next/link";
 
 
 export default function AboutUs(){
 
     return(
-        <div className={"w-[80%] flex flex-col justify-center content-center items-center m-[10px auto] "}>
+        <div className={"w-[90%] flex flex-col justify-center content-center items-center m-[10px auto] "}>
             <h1 className={"font-bold text-2xl font-poppins"}>About author</h1>
             <div className={" mt-20 flex flex-col text-4xl font-medium  leading-17 about__container"}>
                 <div className={"flex"}>
@@ -18,7 +19,7 @@ export default function AboutUs(){
                 </div>
                 <div className={"flex gap-3 justify-between"}>
                     <h2 className={"text-orange-400"}>Nevani Housing.</h2>
-                    <div className={"flex rounded-full p-2 gap-2 border-2 w-fit items-center font-medium text-xl m-3"}>
+                    <div className={"flex rounded-full p-2 gap-2 border-2 w-fit items-center font-medium text-xl m-3 open__to__work"}>
                         <div className={"bg-green-500 rounded-full p-2 h-4 w-4 flex items-center justify-center relative -z-10"}>
                             <div className={"bg-green-100 rounded-full h-2 w-2 absolute"}></div>
                         </div>
@@ -31,7 +32,7 @@ export default function AboutUs(){
                 </div>
             </div>
 
-            <div className={"flex flex-wrap gap-4 [&>*]:rounded-2xl [&>*]:flex-col [&>*]:h-80 [&>*>div]:border [&>*>div]:w-fit [&>*>div]:m-2 [&>*>div]:font-bold [&>*>div]:p-1 [&>*>div]:rounded-2xl  [&>*]:w-80 [&>*]:border mt-32"}>
+            <div className={"m-[0 auto] justify-center content-center w-[65%] flex flex-wrap gap-4 [&>*]:rounded-2xl [&>*]:flex-col [&>*]:h-86 [&>*>div]:border [&>*>div]:w-fit [&>*>div]:m-2 [&>*>div]:font-bold [&>*>div]:p-1 [&>*>div]:rounded-2xl  [&>*]:w-80 [&>*]:border mt-42"}>
                 <div className={"flex flex-col gap-2 "}>
                     <div className={""}>My Experience</div>
                     <div className={"flex flex-col"}>
@@ -47,19 +48,29 @@ export default function AboutUs(){
                         <p>2023- Remote -Part Time</p>
                     </div>
                 </div>
-                <div>
-                    <div className={""}>My Music playlist</div>
+                <div className={"overflow-scroll"}>
+                    <div className={"absolute z-1 bg-orange-500 text-white"}>My Music playlist</div>
+                    <iframe className={"rounded-2xl relative"}
+                            src="https://open.spotify.com/embed/playlist/4x9OtLt7bsmvqktbF0Y0Gm?utm_source=generator&theme=0"
+                            width="100%" height="352" frameBorder="0" allowFullScreen={false}
+                            allow={"autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"}
+                            loading="lazy">
+                    </iframe>
                 </div>
-                <div>
-                    <div className={""}>What i&#39;m reading</div>
+                <div className={"relative"}>
+                    <div className={"absolute z-1 bg-orange-500 text-white"}>What i&#39;m reading</div>
+                    <img src={"https://i.pinimg.com/236x/d3/d2/4b/d3d24bbe8d9e2fe8617da09617ccac0c.jpg"}
+                         alt={"book i read daily"}
+                         className={"w-full h-full p-2"}
+                    ></img>
                 </div>
 
             </div>
             <div className={"flex flex-col mt-20"}>
-                <h2>My tech stack</h2>
-                <div className={"flex flex-wrap w-[50%] gap-4 items-center justify-center m-[auto]"}>
+                <div className={"text-3xl font-bold underline text-center"}>My tech stack</div>
+                <div className={"flex flex-wrap w-[50%] gap-4 items-baseline justify-center m-[auto]"}>
                     {techStack.map((tech,index)=>(
-                        <div key={index} className={"flex flex-col leading-7"}>
+                        <div key={index} className={"flex flex-col leading-7 align-center p-2"}>
                             <Image
                                 src={tech.image}
                                 alt={tech.name}
@@ -67,8 +78,29 @@ export default function AboutUs(){
                                 width={100}
                                 objectFit={"cover"}
                             />
-                            <p>{tech.name}</p>
+                            <p className={"font-medium text-xl"}>{tech.name}</p>
                         </div>
+                    ))}
+                </div>
+
+            </div>
+            <div className={"flex flex-col mt-20"}>
+                <div className={"text-3xl font-bold underline text-center"}>Socials</div>
+                <div className={"flex flex-wrap w-[80%] gap-4 items-baseline justify-center m-[auto]"}>
+                    {socials.map((platform,index)=>(
+                        <Link key={index} href={platform.handleUrl}>
+                            <div key={index} className={"flex flex-col leading-7 align-center p-2 cursor-pointer"}>
+                                <Image
+                                    src={platform.icon}
+                                    alt={platform.site}
+                                    height={100}
+                                    width={100}
+                                    objectFit={"cover"}
+                                />
+                                <p className={"font-medium text-xl"}>{platform.site}</p>
+                            </div>
+                        </Link>
+
                     ))}
                 </div>
 
--- src/utils/techStack.ts
+++ src/utils/techStack.ts
@@ -3,10 +3,10 @@ type Stack ={
     image:string,
     level:string
 }
-const techStack:Stack[] =[
+export const techStack:Stack[] =[
     {name:"Java",image:"https://cdn.svgporn.com/logos/java.svg",level:"Intermediate"},
     {name:"Typescript",image:"https://cdn.svgporn.com/logos/typescript-icon.svg",level:"Intermediate"},
-    {name:"Golang",image:"",level:"Beginner"},
+    {name:"Golang",image:"https://cdn.svgporn.com/logos/goland.svg",level:"Beginner"},
     {name:"Javascript",image:"https://cdn.svgporn.com/logos/javascript.svg",level:"Intermediate"},
     {name:"SQL",image:"https://cdn.svgporn.com/logos/mysql.svg",level:"Intermediate"},
     {name:"Python",image:"https://cdn.svgporn.com/logos/python.svg",level:"Intermediate"},
@@ -16,4 +16,13 @@ const techStack:Stack[] =[
     {name:"Next js",image:"https://cdn.svgporn.com/logos/nextjs-icon.svg",level:"Beginner"}
 
 ]
-export default techStack
+type Socials={
+    site:string,
+    icon:string,
+    handleUrl:string
+}
+export const socials:Socials[]=[
+    {site:"Github",icon:"https://cdn.svgporn.com/logos/github-icon.svg",handleUrl:"https://github.com/johnkinuthiaa"},
+    {site:"Instagram",icon:"https://cdn.svgporn.com/logos/instagram-icon.svg",handleUrl:"https://www.instagram.com/accounts/login/?next=https%3A%2F%2Fwww.instagram.com%2F_sli.ppery_%2F%3Figsh%3DOTcycGYyc3llM3Fz&is_from_rle"},
+
+]
